21c4fe824233a09a2763965bd3ca240425931e87
michael bychkowski <mbychkowski@gmail.com>
2019-08-29 20:08:57 -0400

    configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,40 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-n1l2sadf"
+    BUCKET: "serverless-notes-api-dev-attachmentsbucket-1ejxz0xkjfzm3"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://y4vvt87e4e.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://api.bychnotes.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_RL61I6hFY",
-    APP_CLIENT_ID: "64ao951jo361bu6f4su07birjn",
-    IDENTITY_POOL_ID: "us-east-1:0870fc78-fcfb-4170-8126-f75d21aa9bf5"
+    USER_POOL_ID: "us-east-1_aL6XHJ7T6",
+    APP_CLIENT_ID: "1h5vof877gdm259slset3tmqhr",
+    IDENTITY_POOL_ID: "us-east-1:b692295f-1a19-4635-a55c-f68ca83c06c3"
   }
 };
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "serverless-notes-api-prod-attachmentsbucket-2ygsxny2jtba"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://api.bychnotes.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_P2sUqN2Fk",
+    APP_CLIENT_ID: "1n0lp82jmir226i774v2qhur6t",
+    IDENTITY_POOL_ID: "us-east-1:48026ead-e14f-4392-ba58-a702996ed100"
+  }
+}
+
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
+}
